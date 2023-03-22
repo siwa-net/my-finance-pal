11dfe615a0a79084ae2851ae8ed866e978525315
Simon Wanner <simon.wanner@ext.cobrainer.com>
2023-03-22 10:45:20 +0100

    Content container

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,26 @@
-import '../styles/globals.css';
 import { QueryClient } from '@tanstack/query-core';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { Inter } from 'next/font/google';
 import React from 'react';
 
+import '../styles/globals.css';
 import { Navigation } from '../components/Navigation/Navigation';
+import appStyles from '../styles/app.module.scss';
 
 import type { AppProps } from 'next/app';
 
 const inter = Inter({ subsets: ['latin'] });
 
 const queryClient = new QueryClient();
+
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <QueryClientProvider client={queryClient}>
             <main className={inter.className}>
                 <Navigation />
-                <Component {...pageProps} />
+                <section className={appStyles.ContentWrapper}>
+                    <Component {...pageProps} />
+                </section>
             </main>
         </QueryClientProvider>
     );
